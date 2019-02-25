512c560259788524e50cff9bbce9dcaf092a4f3d
xswei <415516853@qq.com>
2019-02-24 23:59:49 -0800

    add server

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 import os
+from flask_cors import CORS
 
 app = Flask(__name__, instance_relative_config=True)
-
+CORS(app)
+app.jinja_env.variable_start_string = '{{{{'
+app.jinja_env.variable_end_string = '}}}}'
 app.config.from_object('config')
 app.config['WORK_DIR'] = os.getcwd()
 if os.path.exists('instance/config.py'):
--- server/view.py
+++ server/view.py
@@ -1,8 +1,40 @@
 from server import app
 import pickle
 import pymongo
-from flask import render_template,redirect,url_for,abort,send_file,send_from_directory
+from flask import render_template, redirect, url_for, abort, flash, request
+import json
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template("main.html", date="2019-02-23")
+
+
+@app.route('/api/get/<date>')
+def get_entry(date):
+    with open("accounting.json", "r", encoding='utf-8') as fj:
+        table = json.load(fj)
+    entry = table.get(date, {})
+    if entry == {}:
+        entry['status'] = False
+    else:
+        entry['status'] = True
+    return json.dumps(entry)
+
+
+@app.route('/api/set/<date>')
+def set_entry(date):
+    if 'costs' in request.form:
+        with open("accounting.json", "r", encoding='utf-8') as fj:
+            table = json.load(fj)
+        table[date] = request.form.get('costs')
+        with open("accounting.json", "w", encoding='utf-8') as fj:
+            json.dump(fj, table)
+        return True
+    else:
+        return False
+
+
+@app.route('/test')
+def test():
+    return render_template('vue-axios.html')
